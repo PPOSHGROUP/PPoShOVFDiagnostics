0886e4603f99be43935d2fe600b37f22727f318a
Mateusz Czerniawski <mczerniawski@arcon.net.pl>
2018-02-23 15:20:07 +0100

    WIP

--- PPoShOVFDiagnostics/Diagnostics/S2D/Comprehensive/POVF.ClusterNodesConfiguration.Comprehensive.Tests.ps1
+++ PPoShOVFDiagnostics/Diagnostics/S2D/Comprehensive/POVF.ClusterNodesConfiguration.Comprehensive.Tests.ps1
@@ -4,6 +4,7 @@ param(
 )
 
 Describe "Verify Server {$($POVFConfiguration.ComputerName)} in Cluster - {$($POVFConfiguration.ClusterName)} Configuration Status" -Tag 'Configuration' {
+    #<#
     Context "Verify Network Adapter Properties"{
         foreach ($NIC in $POVFConfiguration.NIC) {
             if($NIC.MACAddress) {
@@ -93,10 +94,11 @@ Describe "Verify Server {$($POVFConfiguration.ComputerName)} in Cluster - {$($PO
 
         }
     }
+    #QoS
     if($POVFConfiguration.Team){ 
         Context "Verify Network Team Configuration" {
             foreach ($team in $POVFConfiguration.Team) {
-                $hostTeam = Invoke-Command $session -ScriptBlock {
+                $hostTeam = Invoke-Command $POVFPSSession -ScriptBlock {
                     Get-NetLbfoTeam -Name $USING:team.TeamName
                 }
                 it "Verify Team {$($team.TeamName)} exists" {
@@ -109,26 +111,100 @@ Describe "Verify Server {$($POVFConfiguration.ComputerName)} in Cluster - {$($PO
                     $hostTeam.LoadBalancingAlgorithm | Should Be $team.LoadBalancingAlgorithm
                 }
                 it "Verify Team {$($team.TeamName)} TeamMembers" {
-                    $fromPOVFConfig = $hostTeam.TeamMembers -split ','
-                    Compare-Object -ReferenceObject $fromPOVFConfig -DifferenceObject $hostTeam.TeamMembers | Should -BeNullOrEmpty 
+                    Compare-Object -ReferenceObject $team.TeamMembers -DifferenceObject $hostTeam.Members  | Should -BeNullOrEmpty 
                 }
             }
         }
     }
+    
     if($POVFConfiguration.VmSwitch){
         Context "Verify Virtual Switch Configuration" {
             foreach ($vSwitch in $POVFConfiguration.VmSwitch) {
-                $hostVMSwitch = Invoke-Command $session -ScriptBlock {
+                $hostVMSwitch = Invoke-Command $POVFPSSession -ScriptBlock {
                     Get-VMSwitch -Name $USING:VSwitch.Name
                 }
                 it "Verify vSwitch Name {$($vSwitch.Name)} exists" {
                     $hostVMSwitch | Should -Not -BeNullOrEmpty
                 }
-                it "Verify vSwitch Name {} interface bound" {
+                it "Verify vSwitch Name {$($vSwitch.Name)} interface bound" {
 
                 }
-
+                it "Verify vSwitch Name {$($vSwitch.Name)} minimum bandwith mode - {$($vSwitch.MinimumBandwidthMode)}" {
+                    $hostVMSwitch.BandwidthReservationMode | Should Be $vSwitch.MinimumBandwidthMode
+                }
+                it "Verify vSwitch Name {$($vSwitch.Name)} default bandwidth weight - {$($vSwitch.DefaultFlowMinimumBandwidthWeight)}" {
+                    $hostVMSwitch.DefaultFlowMinimumBandwidthWeight | Should Be $vSwitch.DefaultFlowMinimumBandwidthWeight
+                }
+                $hostVMNetworkAdapters = Invoke-Command $POVFPSSession -ScriptBlock { 
+                    Get-VMNetworkAdapter -SwitchName $USING:vSwitch.Name -ManagementOS:$True
+                }
+                it "Verify vSwitch Name {$($vSwitch.Name)} has network adapters configured - {$(($vSwitch.VMNetworkAdapters).Count)}" { 
+                    $hostVMNetworkAdapters.Count | Should Be ($vSwitch.VMNetworkAdapters).Count
+                }
+                foreach ($vSwitchVMNetworkAdapter in $vSwitch.VMNetworkAdapters) {
+                    it "Verify VMNetworkAdapter {$($vSwitchVMNetworkAdapter.Name)} exists" {
+                        $vSwitchVMNetworkAdapter.Name | Should -BeIn $hostVMNetworkAdapters.Name
+                    }
+                    it "Verify VMNetworkAdapter {$($vSwitchVMNetworkAdapter.Name)} VLANID - {$($vSwitchVMNetworkAdapter.VLANID)}" {
+                        $vlanid = Invoke-Command $POVFPSSession -ScriptBlock {
+                            Get-VMNetworkAdapterVlan -VMNetworkAdapterName $USING:vSwitchVMNetworkAdapter.Name -ManagementOS:$true 
+                        }
+                        $vlanid.AccessVlanId | Should Be $vSwitchVMNetworkAdapter.VLANID
+                    }
+                    it "Verify VMNetworkAdapter {$($vSwitchVMNetworkAdapter.Name)} Minimum Bandwidth Weight - {$($vSwitchVMNetworkAdapter.MinimumBandwidthWeight)}" {
+                        ($hostVMNetworkAdapters.Where({$PSItem.Name -eq $vSwitchVMNetworkAdapter.Name})).BandwidthPercentage | Should Be $vSwitchVMNetworkAdapter.MinimumBandwidthWeight
+                    }
+                    $netAdapterIPConfiguration = Invoke-Command $POVFPSSession -ScriptBlock {
+                        Get-NetIPConfiguration | Where-Object {$PSItem.InterfaceAlias -match $USING:vSwitchVMNetworkAdapter.Name} 
+                    }
+                    it "Verify VMNetworkAdapter {$($vSwitchVMNetworkAdapter.Name)} IP Configuration: IPAddress" {
+                        $netAdapterIPConfiguration.IPv4Address.IPAddress | Should Be $vSwitchVMNetworkAdapter.IPConfiguration.IPAddress
+                    }
+                    it "Verify VMNetworkAdapter {$($vSwitchVMNetworkAdapter.Name)} IP Configuration: DefaultGateway" {
+                        $netAdapterIPConfiguration.IPv4DefaultGateway.NextHop | Should Be $vSwitchVMNetworkAdapter.IPConfiguration.DefaultGateway
+                    }
+                    it "Verify VMNetworkAdapter {$($vSwitchVMNetworkAdapter.Name)} IP Configuration: Prefix" {
+                        $netAdapterIPConfiguration.IPv4Address.PrefixLength | Should Be $vSwitchVMNetworkAdapter.IPConfiguration.PrefixLength
+                    }
+                    it "Verify VMNetworkAdapter {$($vSwitchVMNetworkAdapter.Name)} IP Configuration: DNSClientServerAddress" {
+                        $DNSServers = if(($netAdapterIPConfiguration.DNSServer | Where-Object {$PSItem.AddressFamily -eq '2'}).ServerAddresses) {
+                            #For IPV4 configuration Get-NetIPConfiguration returns AddressFamily -eq 2
+                            ($netAdapterIPConfiguration.DNSServer | Where-Object {$PSItem.AddressFamily -eq '2'} | 
+                                Select-Object -ExpandProperty ServerAddresses).Split(',')
+                        }
+                        else {
+                            $DNSServers = $null
+                        }
+                        #do przerobienia
+                        $DNSServers | Should -BeIn $vSwitchVMNetworkAdapter.IPConfiguration.DNSClientServerAddress
+                    }
+                }
             }
         }
     }
+    #>
+    if($POVFConfiguration.Roles){
+        Context 'Verify Roles configuration' {
+            $hostRoles = Invoke-Command $POVFPSSession -ScriptBlock {
+                Get-WindowsFeature
+            }
+            foreach($presentRole in $POVFConfiguration.Roles.Present.Name) {
+                $presentRole
+                it "Verify role {$presentRole)} is installed" {
+                    $testRole = $hostRoles | Where-Object {$PSItem.Name -eq $presentRole}
+                    $testRole.Installed |Should Be $true
+                }
+            }
+            if($POVFConfiguration.Roles.Absent) { 
+                foreach($absentRole in $POVFConfiguration.Roles.Absent.Name) {
+                    it "Verify role {$($absentRole.Name)} is installed" {
+                        $testRole = $hostRoles | Where-Object {$PSItem.Name -eq $absentRole.Name}
+                        $testRole.Installed |Should Be $false
+                    }
+                }
+            }   
+        }
+    }
+
+    #HyperV config
 }
